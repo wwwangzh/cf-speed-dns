8860a106e4fe8a665efdcea810310ef1ed9f06a2
wwangxa丶 <674065664@qq.com>
2025-05-26 15:08:04 +0800

    Update dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -87,6 +87,7 @@ def push_plus(content):
 def main():
     # 获取最新优选IP
     ip_addresses_str = get_cf_speed_test_ip()
+    print('result: ', ip_addresses_str)
     ip_addresses = ip_addresses_str.split(',')
     dns_records = get_dns_records(CF_DNS_NAME)
     push_plus_content = []
